26baa60d5b703005830fe828f108f842b29e1b31
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2022-12-19 20:11:29 -0300

    att

--- teste3/script.js
+++ teste3/script.js
@@ -8,6 +8,15 @@ function Verificar(){
 
         alert('Verifique os dados e tente novamente')
     }else {
-        
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked){
+            genero = 'homem'
+        }else if (fsex[1].checked){
+            genero = 'mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML=`Detectamos ${genero} com ${idade} anos.`
     }
 }
