265d3ba03759b928d87202c2e382a975cfd02a47
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2023-01-16 20:04:53 -0300

    NEW

--- javascript/ex.011.js
+++ javascript/ex.011.js
@@ -1,13 +1,9 @@
 var idade = 18
-
 if (idade < 16){
-
     console.log(`Você não pode votar`)
 }else if (idade < 18 || idade > 65){
-
     console.log(`Você pode votar (voto opcional)`)
 }else {
-
     console.log(`Você precisa votar(voto obrigatório)`)
 }
 
