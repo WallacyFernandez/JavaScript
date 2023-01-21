ae5b6e813cbb37c4b85020e5e68de37181f7d449
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2023-01-21 20:39:06 -0300

    finish code

--- teste3/script.js
+++ teste3/script.js
@@ -1,46 +1,56 @@
-function Verificar(){
+function Verificar() {
 
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if(fano.value.length == 0 || fano.value > ano){
+    if (fano.value.length == 0 || fano.value > ano) {
 
         alert('Verifique os dados e tente novamente')
-    }else {
+    } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        if (fsex[0].checked){
+        if (fsex[0].checked) {
 
-            genero = 'homem'
+            genero = 'masculino'
 
-            if(idade >= 0 && idade < 10){
+            if (idade >= 0 && idade < 10) {
 
                 img.setAttribute('src', 'homemcriança.png')
-            } else if (idade < 21){
-
+                res.innerHTML = ` <p>Detectamos uma criança do genero ${genero} com ${idade} anos.</p>`
+            } else if (idade < 21) {
+                res.innerHTML = ` <p>Detectamos um adolecente do genero ${genero} com ${idade} anos.</p>`
                 img.setAttribute('src', 'homemadolecente.png')
-            } else if (idade < 50){
-
+            } else if (idade < 50) {
+                res.innerHTML = ` <p>Detectamos um adulto do genero ${genero} com ${idade} anos.</p>`
                 img.setAttribute('src', 'homemadulto.png')
             } else {
-
+                res.innerHTML = ` <p>Detectamos um idoso do genero ${genero} com ${idade} anos.</p>`
                 img.setAttribute('src', 'homemidoso.png')
             }
-        }else if (fsex[1].checked){
-            genero = 'mulher'
-            if(idade >= 0 && idade < 10){
-
+        } else if (fsex[1].checked) {
+            genero = 'feminino'
+            if (idade >= 0 && idade < 10) {
+                res.innerHTML = ` <p>Detectamos uma criança do genero ${genero} com ${idade} anos.</p>`
                 img.setAttribute('src', 'mulhercriança.png')
+            } else if (idade < 21) {
+                res.innerHTML = ` <p>Detectamos um adolecente do genero ${genero} com ${idade} anos.</p>`
+                img.setAttribute('src', 'mulheradolecente.png')
+            } else if (idade < 50) {
+                res.innerHTML = ` <p>Detectamos um adulto do genero ${genero} com ${idade} anos.</p>`
+                img.setAttribute('src', 'mulheradulta.png')
+
+            } else {
+                res.innerHTML = ` <p>Detectamos um idoso do genero ${genero} com ${idade} anos.</p>`
+                img.setAttribute('src', 'mulheridosa.png')
             }
         }
-       
+
     }
 
     res.style.textAlign = 'center'
-    res.innerHTML= ` <p>Detectamos ${genero} com ${idade} anos.</p>`
     res.appendChild(img)
 }
